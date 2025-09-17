7583cdb510043e8ac8f9bb35fbca415d0ede41d6
Mallaklyps <florian.paupitz@gmail.com>
2025-09-17 11:59:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -89,10 +89,14 @@ sudo chmod +x /etc/lightdm/lightdm-xrandr.sh
 # Installiere Nerd-Fonts
 
 
-# Installiere Catppuccin GTK Theme
+# Installiere Catppuccin GTK Theme & Cursor
 git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme.git
 
 ~/debian-qtile/Catppuccin-GTK-Theme/themes/./install.sh --tweaks frappe float -t blue -c dark -l
+
+wget https://github.com/catppuccin/cursors/releases/download/v2.0.0/catppuccin-frappe-dark-cursors.zip
+
+unzip catppuccin-frappe-dark-cursors.zip -d "~/.icons"
 
 
 # Kopiere bashrc
