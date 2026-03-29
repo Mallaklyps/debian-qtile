e18d77a9c03723e420f23a4e5ab68c7873ff8b6b
Mallaklyps <florian.paupitz@gmail.com>
2026-03-29 11:58:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,7 +46,9 @@ sudo apt install -y cmatrix figlet bat fastfetch neomutt calcurse sc-im htop rip
 
 wget https://developer.download.nvidia.com/compute/cuda/repos/debian13/x86_64/cuda-keyring_1.1-1_all.deb
 sudo apt install -y ./cuda-keyring_1.1-1_all.deb
-sudo apt update && sudo apt install -y nvidia-driver nvidia-kernel-open-dkms nvidia-settings libnvoptix1 cuda-toolkit nvidia-driver-libs:i386
+sudo apt update && sudo apt install -y nvidia-driver-pinning-595
+
+sudo apt install -y nvidia-driver nvidia-kernel-open-dkms nvidia-settings libnvoptix1 cuda-toolkit nvidia-driver-libs:i386
 
 echo "panic=0 ro quiet loglevel=2 nvidia-drm.modeset=1 nvidia-drm.fbdev=1" | sudo tee -a /etc/kernel/cmdline
 
